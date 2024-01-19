fee4f6ea86127dba58f86fdfefc604fddda0e98c
AKshay <akshaykurundwade7249@gmail.com>
2024-01-20 00:31:43 +0530

    carousel images added

--- src/customer/components/HomeCarousel/MaincarouselData.js
+++ src/customer/components/HomeCarousel/MaincarouselData.js
@@ -1,12 +1,21 @@
 export const MaincarouselData=[
     {
-        image:"https://www.ethnicplus.in/media/megaplaza/bannerslider/banner/image/1/0/10_5.jpg",
-        path:"women/clothing/lengha_choli"
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_9.jpg",
+        path:"/src/women/clothing/10_9.jpg"
     },
     {
-        image:"https://www.ethnicplus.in/media/megaplaza/bannerslider/banner/image/1/2/12_4.jpg",
-        path:"women/clothing/women_dress"
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_7.jpg",
+        path:"/src/women/clothing/11_7.jpg"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_7.jpg",
+        path:"/src/women/clothing/12_7.jpg"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_7.jpg",
+        path:"/src/women/clothing/11_7 (1).jpg"
     }
 
 ]
+
 
