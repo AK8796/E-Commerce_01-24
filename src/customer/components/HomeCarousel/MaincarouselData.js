143a10335c573e0bab6bf216d2f1da2ab802cb52
AK8796 <akshaykurundwade99@gmail.com>
2024-01-20 15:32:40 +0530

    banner photo changed

--- src/customer/components/HomeCarousel/MaincarouselData.js
+++ src/customer/components/HomeCarousel/MaincarouselData.js
@@ -1,19 +1,19 @@
 export const MaincarouselData=[
     {
         image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_9.jpg",
-        path:"/src/women/clothing/10_9.jpg"
+        path:"/women/clothing(banner)/10_9.jpg"
     },
     {
         image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_7.jpg",
-        path:"/src/women/clothing/11_7.jpg"
+        path:"/women/clothing(banner)/11_7.jpg"
     },
     {
         image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_7.jpg",
-        path:"/src/women/clothing/12_7.jpg"
+        path:"/women/clothing(banner)/12_7.jpg"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_7.jpg",
-        path:"/src/women/clothing/11_7 (1).jpg"
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/3/13_6.jpg",
+        path:"/src/women/clothing(banner)/13_6.jpg"
     }
 
 ]
